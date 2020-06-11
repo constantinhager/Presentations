e3665e42e9b7c65a3ebfebb417eca99d4da2a28b
Constantin Hager <constantin.hager@outlook.de>
2020-06-11 09:48:20 +0200

    changed Account name for the container

--- AzureMeetupHannover-2020-06-11/DeployStateFile/StateFile.ps1
+++ AzureMeetupHannover-2020-06-11/DeployStateFile/StateFile.ps1
@@ -2,9 +2,9 @@ az group create --name meetup-rg --location westeurope --tags creator="Constanti
 
 az storage account create --name meetupterraformstate --resource-group meetup-rg --location westeurope --sku Standard_LRS --kind StorageV2 --tags creator="Constantin Hager" function="tfstate"
 
-az storage container create -n compute --account-name uaiterraformstate
-az storage container create -n databases --account-name uaiterraformstate
-az storage container create -n managementgovernance --account-name uaiterraformstate
-az storage container create -n network --account-name uaiterraformstate
-az storage container create -n storage --account-name uaiterraformstate
-az storage container create -n others --account-name uaiterraformstate
+az storage container create -n compute --account-name meetupterraformstate
+az storage container create -n databases --account-name meetupterraformstate
+az storage container create -n managementgovernance --account-name meetupterraformstate
+az storage container create -n network --account-name meetupterraformstate
+az storage container create -n storage --account-name meetupterraformstate
+az storage container create -n others --account-name meetupterraformstate
