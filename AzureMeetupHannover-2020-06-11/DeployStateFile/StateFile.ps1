b188b9dc4bfd499e213dec92f85f90523f0ee7d2
Constantin Hager <constantin.hager@outlook.de>
2020-06-11 09:47:30 +0200

    Updated Creator

--- AzureMeetupHannover-2020-06-11/DeployStateFile/StateFile.ps1
+++ AzureMeetupHannover-2020-06-11/DeployStateFile/StateFile.ps1
@@ -1,6 +1,6 @@
-az group create --name meetup-rg --location westeurope --tags creator="CH" function="Meetup TF Storage Account for State Files"
+az group create --name meetup-rg --location westeurope --tags creator="Constantin Hager" function="Meetup TF Storage Account for State Files"
 
-az storage account create --name meetupterraformstate --resource-group meetup-rg --location westeurope --sku Standard_LRS --kind StorageV2 --tags creator="CH" function="tfstate"
+az storage account create --name meetupterraformstate --resource-group meetup-rg --location westeurope --sku Standard_LRS --kind StorageV2 --tags creator="Constantin Hager" function="tfstate"
 
 az storage container create -n compute --account-name uaiterraformstate
 az storage container create -n databases --account-name uaiterraformstate
