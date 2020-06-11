ba64d1ea72158e5d2a3417a2fec0f9bc9ca526fd
Constantin Hager <constantin.hager@outlook.de>
2020-06-11 09:46:05 +0200

    changed function

--- AzureMeetupHannover-2020-06-11/DeployStateFile/StateFile.ps1
+++ AzureMeetupHannover-2020-06-11/DeployStateFile/StateFile.ps1
@@ -1,4 +1,4 @@
-az group create --name meetup-rg --location westeurope --tags creator="CH" function="TF Storage Account for State Files"
+az group create --name meetup-rg --location westeurope --tags creator="CH" function="Meetup TF Storage Account for State Files"
 
 az storage account create --name meetupterraformstate --resource-group meetup-rg --location westeurope --sku Standard_LRS --kind StorageV2 --tags creator="CH" function="tfstate"
 
