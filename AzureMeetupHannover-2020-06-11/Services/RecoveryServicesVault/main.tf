f8df7650e7baf1c143642fe64bae2f04e971f9f9
Constantin Hager <constantin.hager@outlook.de>
2020-06-11 10:08:16 +0200

    changed sources of Terraform modules

--- AzureMeetupHannover-2020-06-11/Services/RecoveryServicesVault/main.tf
+++ AzureMeetupHannover-2020-06-11/Services/RecoveryServicesVault/main.tf
@@ -1,5 +1,5 @@
 module "ResourceGroup" {
-  source = "../TerraformModules/ResourceGroup"
+  source = "../global/ResourceGroup"
 
   resource_group_name = "meetupbackup-rg"
   tag_creator         = "Constantin Hager"
@@ -8,7 +8,7 @@ module "ResourceGroup" {
 }
 
 module "RecoveryServicesVault" {
-  source = "../TerraformModules/RecoveryServicesVault"
+  source = "../global/RecoveryServicesVault"
 
   resource_group_name          = module.ResourceGroup.resource_group_name
   recovery_services_vault_name = "meetupbackupvault"
