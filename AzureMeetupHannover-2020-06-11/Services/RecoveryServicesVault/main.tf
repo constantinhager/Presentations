70a84915d1ccfb9795ef2c7026680e07242cdef1
Constantin Hager <constantin.hager@outlook.de>
2020-06-11 09:59:49 +0200

    Updated Recovery Services Vault for Azure Meetup

--- AzureMeetupHannover-2020-06-11/Services/RecoveryServicesVault/main.tf
+++ AzureMeetupHannover-2020-06-11/Services/RecoveryServicesVault/main.tf
@@ -1,22 +1,22 @@
 module "ResourceGroup" {
-  source = "../../../Global/ResourceGroup"
+  source = "../TerraformModules/ResourceGroup"
 
-  resource_group_name = "backup-rg"
+  resource_group_name = "meetupbackup-rg"
   tag_creator         = "Constantin Hager"
   tag_function        = "Resource group for Recovery Services vaults"
-  location            = "North Europe"
+  location            = "West Europe"
 }
 
 module "RecoveryServicesVault" {
-  source = "../../../Global/RecoveryServicesVault"
+  source = "../TerraformModules/RecoveryServicesVault"
 
   resource_group_name          = module.ResourceGroup.resource_group_name
-  recovery_services_vault_name = "uaibackup01"
+  recovery_services_vault_name = "meetupbackupvault"
 
   sku = "Standard"
 
   tag_creator  = "Constantin Hager"
-  tag_function = "Recovery services Vault for UAI"
+  tag_function = "Recovery services Vault for Azure Meetup"
 
-  location = "North Europe"
+  location = "West Europe"
 }
