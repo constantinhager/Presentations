e0af0c42d38bec55527ab776f5b8b1c2cdf23b6b
Constantin Hager <constantin.hager@outlook.de>
2021-06-17 17:16:48 +0200

    added Az.Automation

--- PowerShellUserGroup-2020-07-08/DSC/NeededModules.ps1
+++ PowerShellUserGroup-2020-07-08/DSC/NeededModules.ps1
@@ -2,3 +2,4 @@ Install-Module ActiveDirectoryDSC -Force -Verbose
 Install-Module ComputerManagementDSC -Force -Verbose
 Install-Module StorageDSC -Force -Verbose
 Install-Module xPSDesiredStateConfiguration -Force -Verbose
+Install-Module Az.Automation -Force -Verbose
