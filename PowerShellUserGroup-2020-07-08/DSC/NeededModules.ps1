574fe272b9db12cd683b864058859a85c28034ad
Constantin Hager <constantin.hager@outlook.de>
2021-06-17 18:50:36 +0200

    Removed Az.Automation from NeededModules

--- PowerShellUserGroup-2020-07-08/DSC/NeededModules.ps1
+++ PowerShellUserGroup-2020-07-08/DSC/NeededModules.ps1
@@ -2,4 +2,3 @@ Install-Module ActiveDirectoryDSC -Force -Verbose
 Install-Module ComputerManagementDSC -Force -Verbose
 Install-Module StorageDSC -Force -Verbose
 Install-Module xPSDesiredStateConfiguration -Force -Verbose
-Install-Module Az.Automation -Force -Verbose
