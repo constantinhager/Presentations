0d831f34b23dd6ddd4090186118628c845bda405
Constantin Hager <constantin.hager@outlook.de>
2021-06-19 15:10:35 +0200

    Added AADSCNeededModules.ps1

---
+++ PowerShellUserGroup-2020-07-08/DSC/AADSCNeededModules.ps1
@@ -0,0 +1,39 @@
+$DSCModules = New-Object -TypeName 'System.Collections.Generic.List[System.Object]'
+
+$row1 = [PSCustomObject]@{
+    Name    = 'ActiveDirectoryDSC'
+    Version = '6.0.1'
+}
+
+$row2 = [PSCustomObject]@{
+    Name    = 'ComputerManagementDSC'
+    Version = '8.4.0'
+}
+
+$row3 = [PSCustomObject]@{
+    Name    = 'StorageDSC'
+    Version = '5.0.1'
+}
+
+$row4 = [PSCustomObject]@{
+    Name    = 'xPSDesiredStateConfiguration'
+    Version = '9.0.1'
+}
+
+$DSCModules.Add($row1)
+$DSCModules.Add($row2)
+$DSCModules.Add($row3)
+$DSCModules.Add($row4)
+
+foreach ($module in $DSCModules) {
+    if (-not(Get-AzAutomationModule -AutomationAccountName $(automationAccountName) -Name $module.Name -ResourceGroupName $(automationAccountRGName))) {
+        $splat = @{
+            AutomationAccountName = $(automationAccountName)
+            ResourceGroupName     = $(automationAccountRGName)
+            Name                  = $module.Name
+            ContentLinkUri        = "https://www.powershellgallery.com/api/v2/package/$($module.Name)/$($module.Version)"
+        }
+
+        New-AzAutomationModule @splat
+    }
+}
