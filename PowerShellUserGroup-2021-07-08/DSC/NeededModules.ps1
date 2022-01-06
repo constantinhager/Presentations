e3fc27d2d0a91927bce1a2cff725fd17b5430146
Constantin Hager <constantin.hager@outlook.de>
2022-01-06 10:37:56 +0100

    Added Repository

--- PowerShellUserGroup-2021-07-08/DSC/NeededModules.ps1
+++ PowerShellUserGroup-2021-07-08/DSC/NeededModules.ps1
@@ -1,4 +1,4 @@
-Install-Module ActiveDirectoryDSC -Force -RequiredVersion '6.0.1'
-Install-Module ComputerManagementDSC -Force -RequiredVersion '8.4.0'
-Install-Module StorageDSC -Force -RequiredVersion '5.0.1'
-Install-Module xPSDesiredStateConfiguration -Force -RequiredVersion '9.0.1'
+Install-Module ActiveDirectoryDSC -Force -RequiredVersion '6.0.1' -Repository 'PSGallery' -Verbose
+Install-Module ComputerManagementDSC -Force -RequiredVersion '8.4.0' -Repository 'PSGallery' -Verbose
+Install-Module StorageDSC -Force -RequiredVersion '5.0.1' -Repository 'PSGallery' -Verbose
+Install-Module xPSDesiredStateConfiguration -Force -RequiredVersion '9.0.1' -Repository 'PSGallery' -Verbose
