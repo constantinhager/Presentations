8f5ac0ea44f6b4e1b090b92d7414615ca2022c61
Constantin Hager <constantin.hager@outlook.de>
2022-01-06 10:57:26 +0100

    Corrected Module Check procedure

--- PowerShellUserGroup-2021-07-08/DSC/AADSCNeededModules.ps1
+++ PowerShellUserGroup-2021-07-08/DSC/AADSCNeededModules.ps1
@@ -26,10 +26,41 @@ $DSCModules.Add($row3)
 $DSCModules.Add($row4)
 
 foreach ($module in $DSCModules) {
-    if (Get-AzAutomationModule -ModuleName $module.Name -ModuleVersion $module.Version -ErrorAction SilentlyContinue) {
-        Write-Host "Module $($module.Name) already exists"
+    $splat = @{
+        AutomationAccountName = "$($env:AUTOMATIONACCOUNTNAME)"
+        ResourceGroupName     = "$($env:AUTOMATIONACCOUNTRGNAME)"
+        Name                  = $module.Name
+    }
+    $retval = Get-AzAutomationModule @splat
+
+    if ($null -ne $retval) {
+        Write-Output "Module $($module.Name) already exists. Check if version is the one we need."
+        if ($module.Version -eq $retval.Version) {
+            Write-Output "Module $($module.Name) is already up to date."
+        } else {
+            Write-Output "Module $($module.Name) is not up to date. Updating..."
+
+            $splat = @{
+                AutomationAccountName = "$($env:AUTOMATIONACCOUNTNAME)"
+                ResourceGroupName     = "$($env:AUTOMATIONACCOUNTRGNAME)"
+                Name                  = $module.Name
+                ContentLinkUri        = "https://www.powershellgallery.com/api/v2/package/$($module.Name)/$($module.Version)"
+            }
+            $retval = New-AzAutomationModule @splat
+
+            while ($retval.ProvisioningState -eq 'Creating') {
+                Start-Sleep -Seconds 2
+                Write-Output "Module $($module.Name) is not completely imported."
+                $splat = @{
+                    AutomationAccountName = "$($env:AUTOMATIONACCOUNTNAME)"
+                    ResourceGroupName     = "$($env:AUTOMATIONACCOUNTRGNAME)"
+                    Name                  = $module.Name
+                }
+                $retval = Get-AzAutomationModule @splat
+            }
+        }
     } else {
-        Write-Host "Importing module $($module.Name) into AutomationAccount $($env:AUTOMATIONACCOUNTNAME)"
+        Write-Output "Importing module $($module.Name) into AutomationAccount $($env:AUTOMATIONACCOUNTNAME)"
         $splat = @{
             AutomationAccountName = "$($env:AUTOMATIONACCOUNTNAME)"
             ResourceGroupName     = "$($env:AUTOMATIONACCOUNTRGNAME)"
