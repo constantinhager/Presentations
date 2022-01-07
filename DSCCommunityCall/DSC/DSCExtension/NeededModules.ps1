0b760873f9fbac358e4b0629317fbf9e376645ef
Constantin Hager <constantin.hager@outlook.de>
2022-01-07 10:24:49 +0100

    changed stage and artifact name

--- DSCCommunityCall/DSC/DSCExtension/NeededModules.ps1
+++
@@ -1,4 +0,0 @@
-Install-Module ActiveDirectoryDSC -Force -RequiredVersion '6.0.1' -Repository 'PSGallery' -Verbose
-Install-Module ComputerManagementDSC -Force -RequiredVersion '8.5.0' -Repository 'PSGallery' -Verbose
-Install-Module StorageDSC -Force -RequiredVersion '5.0.1' -Repository 'PSGallery' -Verbose
-Install-Module xPSDesiredStateConfiguration -Force -RequiredVersion '9.1.0' -Repository 'PSGallery' -Verbose
